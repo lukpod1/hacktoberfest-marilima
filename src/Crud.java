d9e408befbdefc718c402e387bbc918d50fcbd2b
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2019-10-24 19:02:13 -0300

    criacao do construtor

--- src/Crud.java
+++ src/Crud.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crud {
     List<String> lista;
 
+    public Crud(List<String> lista) {
+        this.lista = lista;
+    }
     public static void main(String[] args) {
-
+        List<String> l1 = new ArrayList<>();
+        Crud c1 = new Crud(l1);
     }
 }
